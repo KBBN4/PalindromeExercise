6579865c7e76267e24bc648587e537e012feb85e
katherine Buendia <kbbuendia24@gmail.com>
2023-05-03 20:04:46 -0400

    PalindromeExcercise update

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -3,12 +3,36 @@ using Xunit;
 
 namespace PalindromeExerciseTests
 {
-    public class UnitTest1
+    public class IsAPalindromeTest
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("civic", true)]
+        [InlineData("radar", true)]
+        [InlineData(" katherine", false)]
+        [InlineData("madam", true)]
+
+        public void isAPalidromeTest(string word, bool expected)
         {
+            //  arrange 
+            var isAPali = new WordSmith();
+
+            // act
+
+            var actual = isAPali.IsAPalidrone(word,expected);
+
+            // assert
+
+            Assert.Equal(expected, actual);
+
+
 
         }
-    }
+
+    }                                                               // ← you will need to put some test data and expected result here.
+
+    //arrange
+
+
+
+
 }
---
+++ PalindromeExercise/PalindromeExerciseTests/WordSmith.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace PalindromeExerciseTests
+{
+	public class WordSmith
+	{
+		public bool IsAPalidrone(string word, bool expected)
+		{
+			//throw new Exception();
+			string words = "";
+
+			for (int i = word.Length - 1; i >= 0; i--)
+			{
+				words += word[i];
+
+			}
+			var Answer = word == words ? true : false;
+
+			return Answer;
+
+		}
+	}
+}
+
